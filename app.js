4ca68ad961922d54fafa7396d0a2319bda2f6eae
dorultan <dorultanianosgyorgy@gmail.com>
2018-07-05 13:48:25 +0100

    server fix

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 8080);
+app.set('PORT', process.env.PORT || 8080);
 
-app.get('*', (req, res)=> {
-	res.sendFile(__dirname + '/dist/index.html');
+app.use( (req, res)=> {
+	res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 })
 
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(app.get('PORT'), function() {
   console.log('listening on port ', server.address().port);
 });
