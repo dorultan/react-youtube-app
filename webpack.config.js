ca525c9757d0341f58135ad5f80a7871f3986202
dorultan <dorultanianosgyorgy@gmail.com>
2018-07-05 13:54:53 +0100

    s

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config = {
-	mode: 'development',
+	mode: process.env.NODE_ENV || 'development',
 	entry: ['babel-polyfill', path.resolve('app', 'app.js')],
 	output: {
 		path: path.resolve(__dirname, 'dist'),
@@ -40,14 +40,7 @@ const config = {
 			name: '[name].css',
 			chunkFilename: '[id].css'
 		})
-	],
-
-	devServer: {
-		port: process.env.port || 3000,
-		contentBase: './dist',
-		inline: true,
-		historyApiFallback: true
-	}
+	]
 };
 
 module.exports = config;
